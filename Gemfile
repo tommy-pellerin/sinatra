05ecbd86a428d6bfa9b8b144de2cdb35bab8ecf1
tommy-pellerin <tommytp@hotmail.fr>
2024-02-05 10:52:47 +0100

    fin de 2.5

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org' 
+ruby '3.2.2' 
+gem 'rspec' 
+gem 'rubocop' 
+gem 'pry' 
+gem 'dotenv' 
+gem 'sinatra'
+gem 'rackup'
+
+gem "rerun", "~> 0.14.0"
+
+gem "csv", "~> 3.2"
---
+++ lib/gossip.rb
@@ -0,0 +1,28 @@
+require 'bundler'
+Bundler.require
+
+class Gossip
+  attr_accessor :author, :content
+  
+  def initialize(auteur,contenu)
+    @author = auteur
+    @content = contenu
+  end
+
+  def save
+    CSV.open("./db/gossip.csv", "ab") do |csv|
+      csv << [@author, @content]
+    end
+  end
+
+  def self.all
+    all_gossips = [] #on initialise un array vide
+    CSV.read("./db/gossip.csv").each do |csv_line|  # va chercher chacune des lignes du csv do
+      all_gossips << Gossip.new(csv_line[0], csv_line[1]) # crée un gossip avec les infos de la ligne
+    end
+    
+    return all_gossips #Attention on retourne un array rempli d'objets Gossip, ce sont des objets sous forme #<Gossip:0x00007f221578bb60> qu'il faudra traduire en string
+  end
+  
+end
+
